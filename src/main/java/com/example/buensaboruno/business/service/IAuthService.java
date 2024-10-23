30279f0a656f6ff2283c653645bc20b85f3f9487
lauti2311 <lautaro_ismael2010@hotmail.com>
2024-10-23 15:14:32 -0300

    cosas

---
+++ src/main/java/com/example/buensaboruno/business/service/IAuthService.java
@@ -0,0 +1,17 @@
+//package com.example.buensaboruno.business.service;
+//
+//import com.example.buensaboruno.domain.entities.Cliente;
+//import com.example.buensaboruno.domain.entities.Empleado;
+//import com.example.buensaboruno.dto.LoginDTO;
+//import com.example.buensaboruno.dto.ResponseDTO;
+//
+//import java.util.HashMap;
+//
+//public interface IAuthService {
+//    public HashMap<String, String> login(LoginDTO login) throws Exception;
+//    public ResponseDTO register(Empleado empleado) throws Exception;
+//    Empleado getEmpleadoById(Long id);
+//    public HashMap<String, String> loginCliente(LoginDTO login) throws Exception;
+//    public ResponseDTO registerCliente(Cliente cliente) throws Exception;
+//    Cliente getClienteById(Long id);
+//}
--- src/main/java/com/example/buensaboruno/presentation/rest/controller/UsuarioController.java
+++ src/main/java/com/example/buensaboruno/presentation/rest/controller/UsuarioController.java
@@ -6,7 +6,7 @@ import com.example.buensaboruno.domain.entities.Usuario;
 import com.example.buensaboruno.presentation.rest.base.BaseControllerImpl;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
